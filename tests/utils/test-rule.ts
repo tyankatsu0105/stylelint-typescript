a957a2ffd549bc435f11a29a927290fd917e294b
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-05-27 13:10:06 +0900

    test: refactor test environment (#4)
    * test: refactor test environment
    
    * docs: update readme

--- tests/utils/test-rule.ts
+++ tests/utils/test-rule.ts
@@ -1,7 +1,4 @@
-/**
- * Note: If jest-preset-stylelint supports ts-node, this process is not necessary.
- */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const getTestRule = require("jest-preset-stylelint/getTestRule");
 
-export const testRule = getTestRule({ plugins: ["./dist/lib"] });
+export const testRule = getTestRule({ plugins: ["./lib/index.ts"] });
